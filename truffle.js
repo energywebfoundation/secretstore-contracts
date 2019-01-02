5ea73a01a26bf1749810baced7a3ffc999aa2a21
ngyam <ngy.am.zs@gmail.com>
2019-01-02 16:26:14 +0100

    add(deployer): deployer address

--- truffle.js
+++ truffle.js
@@ -9,7 +9,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "401697", // Toablaba network id
-      gas: 7000000
+      gas: 7000000,
+    },
+    deployment: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "401697", // Toablaba network id
+      gas: 7000000,
+      from: "0x569f1153F6939Bb98530EC54e4dec7be481CA80f",
     }
   },
   solc: {
